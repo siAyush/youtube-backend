cded3c3f5121c122f831c0cbd49e4d5ee8cc068b
Ayush <siayush.gupta@gmail.com>
2024-09-17 16:28:52 +0530

    route

--- src/app.js
+++ src/app.js
@@ -10,8 +10,15 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
 
 export default app;
